b8c4843bc1d36a397fc11545c27259dcfeb897e7
h1r0yuki8823 <hiroyukiandswift@gmail.com>
2018-04-18 08:24:31 +0900

    関数を分けて記述

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const http = require('http');
 const fs = require('fs');
 
-var server = http.createServer(
-    (request, response)=>{
-        fs.readFile('./index.html','UTF-8',(error, data) =>{
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(data);
-            response.end();
-        });
-    }
-);
+var server = http.createServer(getFromCliant);
 
 server.listen(3000);
 console.log('Server Start!');
+
+
+function getFromCliant(req, res){
+    request = req;
+    response = res;
+        fs.readFile('./index.html','UTF-8',writeToResponse);    
+}
+
+function writeToResponse(error, data){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+}
